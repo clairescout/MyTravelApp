02faa7d572ef20a68cd599a08ec15978d14663eb
Emily Hales <emilyhales2@gmail.com>
2018-10-09 15:08:09 -0600

    spotify works woot woot

--- app/src/main/java/com/example/clairescout/mytravelapp/VacationFeedActivity.java
+++ app/src/main/java/com/example/clairescout/mytravelapp/VacationFeedActivity.java
@@ -2,28 +2,127 @@ package com.example.clairescout.mytravelapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 public class VacationFeedActivity extends AppCompatActivity {
 
-    @Override
+    private static final String CLIENT_ID = "bb32999f67d1447fa9cb9b4b74e62db5";
+    private static final String REDIRECT_URI = "https://example.com/callback";
+    private String currentSongID = "37i9dQZF1DX7K31D69s4M1";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    private ImageButton playButton;
+    private TextView songName;
+    private TextView songArtist;
+    private FloatingActionButton addMediaButton;
+
+    private boolean paused = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_feed);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        initializeWidgets();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.CONNECTOR.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("VacationFeedActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:" + currentSongID);
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+
+            public void onEvent(PlayerState playerState) {
+                final Track track = playerState.track;
+                if (track != null) {
+                    Log.d("VacationFeedActivity", track.name + " by " + track.artist.name);
+                }
+                songName.setText(track.name);
+                songArtist.setText(track.artist.name);
             }
         });
+    }
+
+    private void pauseMusic() {
+        mSpotifyAppRemote.getPlayerApi().pause();
+    }
+
+    private void initializeWidgets() {
+        playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (paused) {
+                    mSpotifyAppRemote.getPlayerApi().resume();
+                    playButton.setImageResource(R.drawable.pause_accent);
+                    paused = false;
+                }
+                else {
+                    pauseMusic();
+                    playButton.setImageResource(R.drawable.play_accent);
+                    paused = true;
+                }
+            }
+        });
+
+        songName = findViewById(R.id.song_name);
+        songArtist = findViewById(R.id.song_artist);
+
+        addMediaButton = findViewById(R.id.add_media_floating_action_button);
+        addMediaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.CONNECTOR.disconnect(mSpotifyAppRemote);
     }
 
 }
--- app/src/main/java/presenters/VacationFeedPresenter.java
+++ app/src/main/java/presenters/VacationFeedPresenter.java
@@ -1,8 +1,67 @@
 package presenters;
 
-/**
- * Created by clairescout on 9/25/18.
- */
+import android.util.Log;
+
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 public class VacationFeedPresenter {
+
+    private static VacationFeedPresenter single_instance = new VacationFeedPresenter();
+
+    private VacationFeedPresenter() {
+
+    }
+
+    public static VacationFeedPresenter getInstance() {
+        return single_instance;
+    }
+
+    public void connected(SpotifyAppRemote spotifyAppRemote) {
+        // Play a playlist
+        spotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        spotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState().setEventCallback(new Subscription.EventCallback<PlayerState>() {
+
+            public void onEvent(PlayerState playerState) {
+                final Track track = playerState.track;
+                if (track != null) {
+                    Log.d("VacationFeedActivity", track.name + " by " + track.artist.name);
+                }
+            }
+        });
+    }
+
+    public void pauseMusic(SpotifyAppRemote spotifyAppRemote) {
+        spotifyAppRemote.getPlayerApi().pause();
+    }
+
 }
+
+
+//try {
+//        URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+//        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//        try {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String line;
+//        while ((line = bufferedReader.readLine()) != null) {
+//        stringBuilder.append(line).append("\n");
+//        }
+//        bufferedReader.close();
+//        return stringBuilder.toString();
+//        }
+//        finally{
+//        urlConnection.disconnect();
+//        }
+//        }
+//        catch(Exception e) {
+//        Log.e("ERROR", e.getMessage(), e);
+//        return null;
+//        }
